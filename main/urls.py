f0a702f2687e29a1d848b5b95558d41741c5b8ca
Tunahan Tatli <tunahantatl@gmail.com>
2023-01-09 15:09:22 +0300

    Add-logger-Edit-settings-folder-Edit-dotenv

--- main/urls.py
+++ main/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 # Three modules for swagger:
 from rest_framework import permissions 
 from drf_yasg.views import get_schema_view 
@@ -41,4 +42,5 @@ urlpatterns = [
     path("swagger(<format>\.json|\.yaml)", schema_view.without_ui(cache_timeout=0), name="schema-json"), 
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"), 
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
