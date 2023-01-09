23eee519bc290d4d58c807be47a69afb5bcd29a7
Tunahan Tatli <tunahantatl@gmail.com>
2023-01-09 10:22:48 +0300

    add-swagger

--- main/settings.py
+++ main/settings.py
@@ -36,7 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',   
+   # required for serving swagger ui's css/js files
+
+    # Third party apps:
+    'drf_yasg', 
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
