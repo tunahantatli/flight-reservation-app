13f81c1827c0c81aa1c87738dcd7a91ba1df7b2e
Tunahan Tatli <tunahantatl@gmail.com>
2023-01-10 20:04:47 +0300

    last-edit4auth

--- users/apps.py
+++ users/apps.py
@@ -5,5 +5,5 @@ class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
-    def read(self) -> None:
+    def ready(self) -> None:
         import users.signals
